cfa1639c8b62ae98a8376b7664a01e0b50b9335c
Elena5746328 <lenae.efimowa@yandex.ru>
2025-12-22 19:49:50 +0300

    задание по модулю 2

---
+++ classwork/task_3.py
@@ -0,0 +1,7 @@
+meters = float(input("Введите количество метров: "))
+
+print(f"{meters} метров")
+print(f"{meters * 100} сантиметров")
+print(f"{meters * 10} дециметров")
+print(f"{meters * 1000} миллиметров")
+print(f"{meters * 0.000621} милей")
