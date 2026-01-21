c4b199827c6249cad955b826ae17ca4126cbca2b
Elena5746328 <lenae.efimowa@yandex.ru>
2026-01-21 20:42:36 +0300

    feat/выполнение задач

--- module-3.1/cw/task_12.py
+++ module-3.1/cw/task_12.py
@@ -1,8 +1,38 @@
 list = [-5, 6, 9, 0, -3, 35, 44, 23, 90, -65]
 
 sum_negative = sum(x for x in list if x < 0)
+
 sum_even = sum(x for x in list if x % 2 == 0)
+
 sum_odd = sum(x for x in list if x % 2 != 0)
+
+product_index3 = 1
+for i in range(0, len(list), 3):
+    product_index3 *= list[i]
+
+min_val = min(list)
+max_val = max(list)
+min_index = list.index(min_val)
+max_index = list.index(max_val)
+left_index = min(min_index, max_index)
+rigth_index = max(min_index, max_index)
+elements_between = list[left_index + 1 : rigth_index]
+
+if not elements_between:
+    product_between = 0
+else:
+    product_between = 1
+    for x in elements_between:
+        product_between *= x
+
+print(f"Сумма отрицательных чисел: {sum_negative}")
+print(f"Сумма четных чисел: {sum_even}")
+print(f"Сумма нечетных чисел: {sum_odd}")
+print(f"Произведение элементов с индексами кратными 3: {product_index3}")
+print(f"Произведение элементов между минимальным и максимальным элементом: {product_between}")
+
+
+
 
 
 
--- module-3.1/cw/task_13.py
+++ module-3.1/cw/task_13.py
@@ -10,3 +10,17 @@ for num in list:
     if num % 2 != 0:
         even_odd.append(num)
 
+negative_list = []
+for num in list:
+    if num < 0:
+        negative_list.append(num)
+
+positive_list = []
+for num in list:
+    if num > 0:
+        positive_list.append(num)
+
+print(f"Четные числа: {even_list}")
+print(f"Нечетные числа: {even_odd}")
+print(f"Отрицательные числа: {negative_list}")
+print(f"Положительные числа: {positive_list}")
